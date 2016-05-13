efa2e54f4591f14cbe1eb4ab9a4c70cab8c09d39
Brian DOstilio <briand787b@gmail.com>
2016-05-13 18:34:52 -0400

    Added AJAX! Finally!

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,10 @@
-$(function(){
-	$('div[onload]').trigger('onload');
-});
+// $(function(){
+// 	$('div[onload]').trigger('onload');
+// });
 
-function arrangeTable(){
-	document.getElementById('submission_row').className = "col-md-4";
-	document.getElementById('submission_box').className = "col-md-12";
-	document.getElementById('table-row').className = "col-md-8";
-	document.getElementById('notice-box').style.backgroundColor = "transparent";
-}
+// function arrangeTable(){
+// 	document.getElementById('submission_row').className = "col-md-4";
+// 	document.getElementById('submission_box').className = "col-md-12";
+// 	document.getElementById('table-row').className = "col-md-8";
+// 	document.getElementById('notice-box').style.backgroundColor = "transparent";
+// }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,19 @@ class WelcomeController < ApplicationController
 	before_action :whitelist_params
 
   def index 	
-		if params[:name]	
-		 	limit = params[:limit].to_i
+		if params[:name]
+			limit = params[:limit].to_i
 		 	@name = params[:name]
 		 	if @name == '' or limit < 1
 		 		flash[:notice] = "Please fill out all boxes"
 		 		redirect_to index_path  	
 		 	end
 		 	@positive_tweets = Bot.new(@name, limit).get_tweets
+		 	#Everything below here is new
+			respond_to do |format|
+				format.js {puts "HIT JS IN FORMAT 2"}
+				format.html
+			end
 		end
   end
 
