05bac33c91b6861737c657ef5a3711d1dd8a64e7
Brian DOstilio <briand787b@gmail.com>
2016-05-13 20:11:14 -0400

    might have fixed

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 CLIENT = Twitter::REST::Client.new do |config| 
-	config.consumer_key = ENV['consumer_key'] 
-	config.consumer_secret = ENV['consumer_secret'] 
-	config.access_token = ENV['access_token'] 
-	config.access_token_secret = ENV['access_secret'] 
+	config.consumer_key = '1nZ381aZiAlJ5snuVCsSMGMkD'  #ENV['consumer_key'] 
+	config.consumer_secret = 'smVhBal0RdZNN7DlXXlJq94UIvkcEextzq8UGkceHkLMjciC9E' #ENV['consumer_secret'] 
+	config.access_token = '728601788367015936-xbIwWIwsbzAMSaPFsM7SHHwCLASraki' #ENV['access_token'] 
+	config.access_token_secret = 'QpqobeAETXLKS7Q6Jp8k1w9IpA1P7ZcRXfMI2Sdn76qN0' #ENV['access_secret'] 
 end
